f69249e8a74d2ace92d603cb0762368e79218aa9
MrTadex <tadexqx@gmail.com>
2024-01-27 19:38:36 +0100

    walk

--- scripts/objectMakeSound.gd
+++ scripts/objectMakeSound.gd
@@ -1,6 +1,6 @@
 extends StaticBody3D
 
-@onready var _audio = $AudioStreamPlayer3D
+@onready var _audio = preload("res://resources/sound_music/fall_bonk_1.ogg")
 
 func clicked():
 	_audio.playing = true
