0071ca28e4eb81400898fedab73ece58c451a99f
MrTadex <tadexqx@gmail.com>
2024-01-27 20:08:22 +0100

    music

--- scripts/objectMakeSound.gd
+++ scripts/objectMakeSound.gd
@@ -1,6 +1,20 @@
 extends StaticBody3D
 
-@onready var _audio = preload("res://resources/sound_music/fall_bonk_1.ogg")
+@onready var _audio = $AudioStreamPlayer3D
+#@onready var _audio_player = $AudioStreamPlayer3D
+@onready var audio_repeat:int = 1
+
+var array = [
+				preload("res://resources/sound_music/player_sound/script_29_vroom.ogg")
+			]
 
 func clicked():
+	audio_repeat = 1
 	_audio.playing = true
+
+func _on_audio_stream_player_3d_finished():
+	if audio_repeat == 1:
+		_audio.stream = array[0]
+		_audio.volume_db = 10
+		_audio.playing = true
+		audio_repeat = 0
