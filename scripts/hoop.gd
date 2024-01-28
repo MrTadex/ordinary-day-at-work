9330d5f5c50e3974e0df2cb00a58d87d00e3803c
DavidJan06 <David.jan06@gmail.com>
2024-01-28 10:29:05 +0100

    Main + Bounds for ball

--- scripts/hoop.gd
+++ scripts/hoop.gd
@@ -3,6 +3,7 @@ extends Area3D
 var count = 0
 
 @export var textLable:Label3D
+@export var textLable1:Label3D
 
 func _on_body_entered(body):
 	print(body.name)
@@ -10,3 +11,4 @@ func _on_body_entered(body):
 		print("Goal")
 		count += 1
 		textLable.text = str(count)
+		textLable1.text = str(count)
