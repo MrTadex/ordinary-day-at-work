2b8603d3e22c124272cc94f503c3243527ad1cce
DavidJan06 <David.jan06@gmail.com>
2024-01-28 11:36:04 +0100

    Logger + Ball fix

--- scripts/logger.gd
+++ scripts/logger.gd
@@ -2,6 +2,18 @@ extends Node
 
 @export var items_on_shelfs:int = 0
 
+@export var Start:bool = false
+@export var CanStopAlarm:bool = false
+
+func _ready():
+	add_to_group("EventListeners")
+
+func _on_event(eventName):
+	match eventName:
+		"Start":
+			Start = true
+		"Can Stop Alarm":
+			CanStopAlarm = true
 
 func _process(_delta):
 	#print(items_on_shelfs)
--- scripts/switch.gd
+++ scripts/switch.gd
@@ -5,20 +5,18 @@ extends StaticBody3D
 @onready var _animator = $AnimatedSprite3D
 
 func clicked():
-	if _animator.animation == "On":
-		_animator.play("Off")
-		_light.visible = false
-		get_tree().call_group("EventListeners", "_on_event", "Stop Alarm")
-	elif _animator.animation == "Off":
-		_animator.play("On")
-		_light.visible = true
-		get_tree().call_group("EventListeners", "_on_event", "Start Alarm")
+	if Logger.CanStopAlarm:
+		if _animator.animation == "On":
+			_animator.play("Off")
+			_light.visible = false
+			get_tree().call_group("EventListeners", "_on_event", "Stop Alarm")
+		elif _animator.animation == "Off":
+			_animator.play("On")
+			_light.visible = true
+			get_tree().call_group("EventListeners", "_on_event", "Start Alarm")
 
 func _on_event(eventName):
-	if eventName == "Start" and _animator.animation == "Off":
-		get_tree().call_group("EventListeners", "_on_event", "Stop Alarm")
-		
-	if eventName == "Start" or eventName == "Start Alarm":
+	if eventName == "Start Alarm" or eventName == "Start":
 		_light.visible = true
 	
 	if eventName == "Stop Alarm":
