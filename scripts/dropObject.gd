e94d8ef7967350b60a2647c49ce1c95397556261
MrTadex <tadexqx@gmail.com>
2024-01-28 09:58:06 +0100

    objects

--- scripts/dropObject.gd
+++ scripts/dropObject.gd
@@ -11,3 +11,35 @@ func _on_body_entered(_body):
 	_audio.stream = array[randi_range(0,1)]
 	_audio.volume_db = 35
 	_audio.playing = true
+
+
+func _on_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_body_entered(body):
+	pass # Replace with function body.
